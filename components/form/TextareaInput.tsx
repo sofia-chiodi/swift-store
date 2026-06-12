0b2811957b3d5fc6acd64e95a4598a809b73796e
sofia-chiodi <chiodi.sb@gmail.com>
2026-06-12 17:28:20 -0300

    Adding Create Product action to admin Dashboard

---
+++ components/form/TextareaInput.tsx
@@ -0,0 +1,28 @@
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
+type TextareaInputProps = {
+  name: string;
+  labelText?: string;
+  defaultValue?: string;
+};
+
+function TextareaInput({ name, labelText, defaultValue }: TextareaInputProps) {
+  return (
+    <div className='mb-2'>
+      <Label htmlFor={name} className='capitalize'>
+        {labelText || name}
+      </Label>
+      <Textarea
+        id={name}
+        name={name}
+        defaultValue={defaultValue}
+        rows={5}
+        required
+        className='leading-loose'
+      />
+    </div>
+  );
+}
+
+export default TextareaInput;
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,5 +1,23 @@
+"use server";
+
 import db from "@/utils/db";
+import { currentUser } from "@clerk/nextjs/server";
+import { log } from "console";
 import { redirect } from "next/navigation";
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) redirect("/");
+  return user;
+};
+
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+
+  return {
+    message: error instanceof Error ? error.message : "An error has occurred",
+  };
+};
 
 export const fetchFeaturedProduct = async () => {
   const products = await db.product.findMany({
@@ -32,4 +50,21 @@ export const fetchSingleProduct = async (productId: string) => {
   });
   if (!product) redirect("/products");
   return product;
+};
+
+export const createProductAction = async (
+  prevState: any,
+  formData: FormData,
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+
+  try {
+    const rawData = Object.fromEntries(formData);
+
+    log(rawData);
+
+    return { message: "product created" };
+  } catch (error) {
+    return renderError(error);
+  }
 };
